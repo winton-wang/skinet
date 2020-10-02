30c8eb52975a1c5924ddd784a2e1cba1a518834f
Winton Wang <winton_wang@yahoo.com>
2020-10-01 23:36:42 -0400

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
 
 namespace API
 {
@@ -29,6 +30,8 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllers();
 
              services.AddDbContext<StoreContext>(x =>
