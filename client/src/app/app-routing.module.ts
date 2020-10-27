cd89df8dc89a576a62d394b20493dd907be9f1a4
Winton Wang <winton_wang@yahoo.com>
2020-10-27 15:30:55 -0400

    End of section 17

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ShopComponent } from './shop/shop.component';
 import { TestErrorComponent } from './core/test-error/test-error.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
 import { NotFoundComponent } from './core/not-found/not-found.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 
 const routes: Routes = [
@@ -19,8 +20,15 @@ const routes: Routes = [
   { path: 'basket', loadChildren: () => import('./basket/basket.module').then( mod => mod.BasketModule),
     data: {breadcrumb: 'Basket'}},
 
-  { path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then( mod => mod.CheckoutModule),
-    data: {breadcrumb: 'Checkout'}},
+  { path: 'checkout',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./checkout/checkout.module').then( mod => mod.CheckoutModule),
+    data: {breadcrumb: 'Checkout'}
+  },
+
+
+  { path: 'account', loadChildren: () => import('./account/account.module').then( mod => mod.AccountModule),
+    data: {breadcrumb: {skip: true}}},
 
 
   { path: '**',  redirectTo: 'not-found',  pathMatch: 'full' }
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit} from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 
 
@@ -11,20 +12,38 @@ import { BasketService } from './basket/basket.service';
 export class AppComponent implements OnInit {
   title = 'Skinet';
 
-  constructor(private basketService: BasketService ) { }
+  constructor(private basketService: BasketService, private accountService: AccountService ) { }
 
-  ngOnInit(): void { 
-    
-      const basketId = localStorage.getItem('basket_id');
-
-      if ( basketId ){
-        this.basketService.getBasket(basketId).subscribe( () => {
-          console.log('initialized basket ');
-        }, error => {
-          console.log(error);
-        });
-      }
+  ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
   }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem("token");
+    
+      this.accountService.loadCurrentUser(token).subscribe( () => {
+        console.log('loaded user');
+      }, error => {
+        console.log(error);
+      });
+    
+  }
+
+
+  loadBasket() {
+    const basketId = localStorage.getItem('basket_id');
+
+    if ( basketId ){
+      this.basketService.getBasket(basketId).subscribe( () => {
+        console.log('initialized basket ');
+      }, error => {
+        console.log(error);
+      });
+    }
+
+  }
+
 
 
 }
