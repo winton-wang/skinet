73f211d76e9df02c231d1fb01e5f36c17e8f9ac2
Winton Wang <winton_wang@yahoo.com>
2020-11-01 00:50:53 -0400

    End of section 18 , left

--- API/Dtos/OrderToReturnDto.cs
+++ API/Dtos/OrderToReturnDto.cs
@@ -6,15 +6,15 @@ namespace API.Dtos
 {
     public class OrderToReturnDto
     {
-        public int Id {get; set;}
-        public string BuyerEmail {get; set;}
-        public DateTimeOffset OrderDate { get; set;} 
-        public Address ShipToAddress {get; set; }
-        public string DeliveryMethod {get; set; }
-        public decimal ShippingPrice {get; set; }
-        public IReadOnlyList<OrderItem> OrderItems {get; set;}
-        public decimal Subtotal {get; set;}
-        public decimal Total {get; set; }
-        public string Status {get; set;} 
+        public int Id { get; set; }
+        public string BuyerEmail { get; set; }
+        public DateTimeOffset OrderDate { get; set; }
+        public Address ShipToAddress { get; set; }
+        public string DeliveryMethod { get; set; }
+        public decimal ShippingPrice { get; set; }
+        public IReadOnlyList<OrderItemDto> OrderItems { get; set; }
+        public decimal Subtotal { get; set; }
+        public decimal Total { get; set; }
+        public string Status { get; set; }
     }
 }
