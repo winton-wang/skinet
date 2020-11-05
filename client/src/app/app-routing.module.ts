3c061a487587629304185213cfae751cb453a627
Winton Wang <winton_wang@yahoo.com>
2020-11-05 16:21:00 -0500

    End of Section 20

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -26,10 +26,19 @@ const routes: Routes = [
     data: {breadcrumb: 'Checkout'}
   },
 
+  {
+    path: 'orders',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./orders/orders.module')
+      .then( mod => mod.OrdersModule), data: { breadcrumb: 'Orders'}
+  },
 
-  { path: 'account', loadChildren: () => import('./account/account.module').then( mod => mod.AccountModule),
-    data: {breadcrumb: {skip: true}}},
-
+  {
+    path: 'account',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./account/account.module').then( mod => mod.AccountModule),
+    data: {breadcrumb: {skip: true}}
+  },
 
   { path: '**',  redirectTo: 'not-found',  pathMatch: 'full' }
 
